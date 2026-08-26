7e7fedf94279995e9308e9b337fcf949a541dc2c
Kyant <76829190+Kyant0@users.noreply.github.com>
2026-08-26 14:27:48 +0800

    Bump to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kyant.amdl"
         minSdk = 30
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         androidResources.localeFilters += arrayOf("zh-rCN")
         ndk {
             //noinspection ChromeOsAbiSupport
